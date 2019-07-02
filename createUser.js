a249c2061f954e4e4fec41b53a80c036ccfc57a2
DWONeill18 <50720746+DWONeill18@users.noreply.github.com>
2019-07-02 17:15:57 +0100

    Update createUser.js

--- createUser.js
+++ createUser.js
@@ -7,7 +7,7 @@ let app = express();
 
 //express middlewear
 app.listen(8080, () => {
-    console.log('Listening on port 8008!')
+    console.log('Listening on port 8080!')
 });
 
 //connect to MongoDB
