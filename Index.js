741ab0c2ea02ac28249748372a6ef3a5678a4b3f
Adrian Jones <Nboaram@users.noreply.github.com>
2019-07-04 15:28:00 +0100

    User Routes Structure for Methods to be built off.
    User Schema
    User Routes

--- Index.js
+++ Index.js
@@ -1,2 +1,23 @@
-const userRoutes = require("./user-routes.js");
-const matchRoutes = require("./match-routes.js");
+const userRoutes = require("./user-routes");
+//const matchRoutes = require("./match-routes.js");
+const mongoose = require("mongoose");
+const express = require("express");
+const cors = require('cors');
+const config = require('./config');
+const app = express();
+
+app.use(cors({origin: true}));
+app.use('/user', userRoutes);
+
+
+
+mongoose.connect(config.app.MONGODB_URI, {useNewUrlParser:true}, function (err) {
+    if (err) {
+        console.log('Connection Failed');
+    } else {
+        console.log('Connection Established');
+        app.listen(config.app.PORT, () => {
+            console.log(`Server listening on port: ${config.app.PORT}`);
+        });
+    }
+});
--- user-routes.js
+++ user-routes.js
@@ -1,4 +1,37 @@
-exports.create = require("./createUser.js");
-exports.read = require("./userRead.js");
-exports.update = require("./userUpdate.js");
-exports.delete = require("./userDelete.js");
+const express = require('express');
+const router = express.Router();
+//createUser = require("./createUser.js");
+const readUser = require("./readUser.js");
+//updateUser = require("./userUpdate.js");
+//deleteUser = require("./userDelete.js");
+
+//Make function in another file and export it
+//Join it up in here
+
+
+
+//In the other file the code looks like:
+// exports = router.post('/create', (req, res, next) => {
+//     const newUser = new Users.UserModel(req.body);
+//     return newUser.save().then(
+//         doc => res.status(201).send(doc),
+//         error => next(error)
+//     );
+// });
+
+// Create User
+//router.post('/', createUser);
+
+// Read One User
+router.get('/', readUser);
+
+//Update User
+//router.put('/', updateUser);
+
+// Delete User
+//router.delete('/', deleteUser);
+
+
+module.exports = router;
+
+
